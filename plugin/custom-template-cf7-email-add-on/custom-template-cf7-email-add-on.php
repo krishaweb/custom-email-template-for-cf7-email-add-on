32598c9c87061fd65a95364ba871067981dd16c6
Parth Pandya <parth@krishaweb.com>
2020-07-15 12:12:55 +0530

    Added: Plugin Demo.

---
+++ plugin/custom-template-cf7-email-add-on/custom-template-cf7-email-add-on.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Plugin Name: Custom Email Template For CF7 Email Add on
+ * Plugin URI: 
+ * Description: Contact Form 7 Email Add on plugin provides the responsive Email templates to admin and users.
+ * Version: 1.0
+ * Author: KrishaWeb
+ * Author URI: https://www.krishaweb.com
+ * Text Domain: custom-template-cf7-email-add-on
+ * Domain Path: /languages
+ */
+
+// If check wordpress install or not.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Define add on path.
+if ( ! defined( 'CF7_EMAIL_ADDON_PATH' ) ) {
+	define( 'CF7_EMAIL_ADDON_PATH', plugin_dir_path( __FILE__ ) );
+}
+
+// Define add on URL.
+if ( ! defined( 'CF7_EMAIL_ADDON_URL' ) ) {
+	define( 'CF7_EMAIL_ADDON_URL', plugin_dir_url( __FILE__ ) );
+}
+
+/**
+ * Plugin activate hook.
+ */
+function cs_cf7_email_add_on_activate() {
+	// If check contact form 7 activate or not.
+	if ( ! class_exists( 'CF7_PRO_Email_Add_on' ) ) {
+		// Deactivate contact form 7 plguin.
+		deactivate_plugins( plugin_basename( __FILE__ ) );
+		// Display error message.
+		wp_die( __( 'Please activate Contact Form 7 Email Add On Pro.', 'custom-template-cf7-email-add-on' ), 'Plugin dependency check',
+			array(
+				'back_link' => true,
+			)
+		);
+	}
+}
+register_activation_hook( __FILE__, 'cs_cf7_email_add_on_activate' );
+
+/**
+ * Plugin deactivate hook.
+ */
+function cs_cf7_email_add_on_deactivate() {
+	// Code here.
+}
+register_deactivation_hook( __FILE__, 'cs_cf7_email_add_on_deactivate' );
+
+/**
+ * Plugin uninstall hook.
+ */
+function cs_cf7_email_add_on_uninstall() {
+	// Code here.
+}
+register_uninstall_hook( __FILE__, 'cs_cf7_email_add_on_uninstall' );
+
+/**
+ * Loads a cf 7 email add on textdomain.
+ */
+function cs_cf7() {
+	load_plugin_textdomain( 'custom-template-cf7-email-add-on', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+add_action( 'plugins_loaded', 'cs_cf7' );
+
+/**
+ * Add custom email template.
+ *
+ * @param array $templates Email templates.
+ * @return array
+ */
+function cs_cf7_add_templates( $templates ) {
+	// User templates.
+	$user_templates = array(
+		array(
+			'name' => __( 'Demo Default', 'custom-template-cf7-email-add-on' ),
+			'slug' => 'demo-default',
+			'preview_image' => plugin_dir_url( __FILE__ ) . 'assets/images/user-preview.jpg',
+		),
+	);
+	$templates['user_templates'] = array_merge( $user_templates, $templates['user_templates'] );
+
+	// Admin templates.
+	$admin_templates = array(
+		array(
+			'name' => __( 'Demo Default', 'custom-template-cf7-email-add-on' ),
+			'slug' => 'demo-default',
+			'preview_image' => plugin_dir_url( __FILE__ ) . 'assets/images/admin-preview.jpg',
+		),
+	);
+	$templates['admin_templates'] = array_merge( $admin_templates, $templates['admin_templates'] );
+	return $templates;
+}
+add_filter( 'cf7_email_templates', 'cs_cf7_add_templates' );
---
+++ plugin/custom-template-cf7-email-add-on/templates/user/demo-default.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" 
+xmlns:v="urn:schemas-microsoft-com:vml"
+xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0 " />
+	<meta name="format-detection" content="telephone=no"/>
+	<title>[_site_title]</title>
+	<style type="text/css">
+		body {
+			margin: 0 !important;
+			padding: 0 !important;
+			-webkit-text-size-adjust: 100% !important;
+			-ms-text-size-adjust: 100% !important;
+			-webkit-font-smoothing: antialiased !important;
+		}
+		img {
+			border: 0 !important;
+			outline: none !important;
+		}
+		table {
+			border-collapse: collapse;
+			mso-table-lspace: 0px;
+			mso-table-rspace: 0px;
+		}
+		td, a, span {
+			border-collapse: collapse;
+			mso-line-height-rule: exactly;
+		}
+		p {
+			Margin: 0px !important;
+			Padding: 0px !important;
+		}
+		.em_defaultlink a {
+			color: inherit !important;
+			text-decoration: none !important;
+		}
+		.ExternalClass * {
+			line-height: 100%;
+		}
+		span.MsoHyperlink {
+			mso-style-priority: 99;
+			color: inherit;
+		}
+		span.MsoHyperlinkFollowed {
+			mso-style-priority: 99;
+			color: inherit;
+		}
+		@media only screen and (max-width:900px) {
+			.main_table{ width: 100% !important; }
+		}
+		@media only screen and (max-width:600px) {
+			.table_responsive{ width: 100% !important; }
+			.res_hide{ display: none !important; }
+			.pad_20{ padding: 20px 0 !important; }
+			.font1{ font-size: 18px !important; line-height: 31px !important; padding-top: 25px !important; }
+			.bg_image{ background-size: cover !important; }
+			.pt_0{ padding-top: 0 !important; }
+			.pb_25{ padding-bottom: 25px !important; }
+		}
+	</style>
+<!--[if gte mso 9]>
+  <xml>
+    <o:OfficeDocumentSettings>
+      <o:AllowPNG/>
+      <o:PixelsPerInch>96</o:PixelsPerInch>
+    </o:OfficeDocumentSettings>
+  </xml>
+<![endif]-->
+</head>
+
+<body style="margin:0px; padding:0px;" bgcolor="#ffffff">
+
+	<!--Full width table start-->
+	<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" bgcolor="#ffffff" class="main_table">
+		<!--Template Body Start-->
+		<tr>
+			<td align="center" valign="top">
+				<table width="900" border="0" cellspacing="0" cellpadding="0" align="center" style="table-layout:fixed;width:900px;" class="main_table">
+					<tr>
+						<td align="center" valign="top" bgcolor="#ffffff" style="font-size: 0; line-height: 0; padding: 38px 0px 32px;" class="pad_20"><a href="[_site_url]" style="color: #000; text-decoration: none; display: inline-block;" target="_blank"><img src="[_cf7_email_logo]" border="0" alt="[_site_title]" style="font-family: Verdana, Arial, sans-serif; font-size: 14px; line-height: 18px; color: #000; display: block; border: none; max-width: 100%;" width="238" /></a></td>
+					</tr>
+					<tr>
+						<td align="center" valign="top" style="background-image: url('[cf7_email_addon_url]assets/images/bg_image.jpg'); background-repeat: no-repeat; background-size: cover; background-position: top center; background-color: #FCDCDB;" height="498" class="bg_image">
+            <!--[if gte mso 9]>
+            <v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:900px;height:498px;">
+              <v:fill type="tile" src="[cf7_email_addon_url]assets/images/bg_image.jpg" color="#FCDCDB" />
+              <v:textbox inset="0,0,0,0">
+          <![endif]-->
+          <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+          	<tr>
+          		<td align="center" valign="top" style="padding: 0 15px;">
+          			<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" style="max-width:600px;">
+          				<tr>
+          					<td align="left" valign="top" style="font-family: Verdana, Arial, sans-serif; font-size: 27px; line-height: 40px; color: #1C1B18; font-weight: bold; text-align: left; padding-top: 105px; padding-bottom: 4px;" class="font1">Hi [user-name], <br />Thanks for getting in touch</td>
+          				</tr>
+          				<tr>
+          					<td align="left" valign="top" style="font-family: Verdana, Arial, sans-serif; font-size: 14px; line-height: 24px; color: #656565; text-align: left; padding-bottom: 44px;" class="font2 pb_25">Thanks for getting in touc Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</td>
+          				</tr>
+          				<tr>
+          					<td align="left" valign="top" style="font-size: 0; line-height: 0; padding-bottom: 79px;" class="pb_25"><a href="[blog_page]" style="color: #000; text-decoration: none; display: inline-block;" target="_blank"><img src="[cf7_email_addon_url]assets/images/cta_btn.png" border="0" alt="VISIT OUR BLOG" style="font-family: Verdana, Arial, sans-serif; font-size: 14px; line-height: 18px; color: #000; display: block; border: none; max-width: 100%;" width="184" /></a></td>
+          				</tr>
+          				<tr>
+          					<td align="left" valign="top">
+          						<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+          							<tr>
+          								<td align="center" valign="top">
+          									<?php if ( $this->option ) : ?>
+          									<table border="0" cellspacing="0" cellpadding="0" align="left" class="table_responsive">
+          										<tr>
+          											<td align="left" valign="top" style="padding-bottom: 17px;">
+          												<table border="0" cellspacing="0" cellpadding="0" align="left">
+          													<tr>
+          														<?php if ( $this->option['cf7_facebook'] ) : ?>
+          															<td align="center" valign="top" style="font-size: 0px; line-height: 0px;"><a href="[_cf7_email_facebook]" style="color: #ffffff; text-decoration: none;" target="_blank"><img src="[cf7_email_addon_url]assets/images/fb.png" alt="FB" border="0" style="font-family: Verdana, Arial, helvetica, sans-serif; font-size: 10px; line-height: 14px; font-weight: bold; color: #ffffff; text-transform: uppercase; letter-spacing: 0.02em; max-width: 100%; border: none; display: block;" width="35" /></a></td>
+          														<?php endif; ?>
+          														<?php if ( $this->option['cf7_twitter'] ) : ?>
+          															<td width="14" style="font-size: 1px; line-height: 1px;"></td>
+          															<td align="center" valign="top" style="font-size: 0px; line-height: 0px;"><a href="[_cf7_email_twitter]" style="color: #ffffff; text-decoration: none;" target="_blank"><img src="[cf7_email_addon_url]assets/images/twitter.png" alt="TW" border="0" style="font-family: Verdana, Arial, helvetica, sans-serif; font-size: 10px; line-height: 14px; font-weight: bold; color: #ffffff; text-transform: uppercase; letter-spacing: 0.02em; max-width: 100%; border: none; display: block;" width="35" /></a></td>
+          														<?php endif; ?>
+          														<?php if ( $this->option['cf7_linkdin'] ) : ?>
+          															<td width="14" style="font-size: 1px; line-height: 1px;"></td>
+          															<td align="center" valign="top" style="font-size: 0px; line-height: 0px;"><a href="[_cf7_email_linkdin]" style="color: #ffffff; text-decoration: none;" target="_blank"><img src="[cf7_email_addon_url]assets/images/linkedin.png" alt="IN" border="0" style="font-family: Verdana, Arial, helvetica, sans-serif; font-size: 10px; line-height: 14px; font-weight: bold; color: #ffffff; text-transform: uppercase; letter-spacing: 0.02em; max-width: 100%; border: none; display: block;" width="35" /></a></td>
+          														<?php endif; ?>
+          														<?php if ( $this->option['cf7_pinterest'] ) : ?>
+          															<td width="14" style="font-size: 1px; line-height: 1px;"></td>
+          															<td align="center" valign="top" style="font-size: 0px; line-height: 0px;"><a href="[_cf7_email_pinterest]" style="color: #ffffff; text-decoration: none;" target="_blank"><img src="[cf7_email_addon_url]assets/images/pinterest.png" alt="P" border="0" style="font-family: Verdana, Arial, helvetica, sans-serif; font-size: 10px; line-height: 14px; font-weight: bold; color: #ffffff; text-transform: uppercase; letter-spacing: 0.02em; max-width: 100%; border: none; display: block;" width="35" /></a></td>
+          														<?php endif; ?>
+          													</tr>
+          												</table>
+          											</td>
+          										</tr>
+          									</table>
+          									<?php endif; ?>
+          									<table border="0" cellspacing="0" cellpadding="0" align="right" class="table_responsive">
+          										<tr>
+          											<td align="left" valign="top" style="font-family: Verdana, Arial, sans-serif; font-size: 12px; line-height: 15px; color: #1C1B18; text-align: left; padding-top: 5px;" class="pt_0">&copy; [date_year] <a href="[_site_url]" style="text-decoration: none; color: #1C1B18;" target="_blank">[_site_title]</a></td>
+          										</tr>
+          									</table>
+          								</td>
+          							</tr>
+          						</table>
+          					</td>
+          				</tr>
+          			</table>
+          		</td>
+          	</tr>
+          </table>
+            <!--[if gte mso 9]>
+              </v:textbox>
+            </v:rect>
+        <![endif]-->
+    </td>
+</tr>
+</table>
+</td>
+</tr>
+<!--Template Body End-->
+</table>
+<!--Full width table End--> 
+
+<div style="white-space:nowrap;font:20px courier;color:#ffffff;"><span class="res_hide">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</span></div>
+</body>
+</html>
